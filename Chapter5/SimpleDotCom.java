aa326cffb970e4488df47ecaa21d2a1ceaff69b1
Unicornelia <ez.kornelia@gmail.com>
2017-02-23 10:13:49 +0000

    working more on the simple dot com game

--- Chapter5/SimpleDotCom.java
+++ Chapter5/SimpleDotCom.java
@@ -1,4 +1,34 @@
 class SimpleDotCom {
+
+  int guess = Integer.parseInt(String stringGuess);
+  // convert the string to an integer
+
+  String result = "miss";
+  // make a variable to hold the result we will return
+
+  for (int cell : locationCells) {
+    // repeat with each cell in the locationcell array
+    if (guess == cell) {
+      // compare the user guess to the element in the array
+      result  = "hit";
+      numOfHits++;
+      // we got a hit, increment the numbers
+      break;
+      //get out of the loop
+    }
+  }
+
+  if (numOfHits == locationCells.length()) {
+    result = "kill";
+  }
+
+  System.out.println(result);
+  //display result for user
+
+  return result;
+  //return the result back to the calling method
+
+
   int[] locationCells;
   int numOfHits = 0;
 
--- Chapter5/SimpleDotComTestDrive.java
+++ Chapter5/SimpleDotComTestDrive.java
@@ -1,5 +1,7 @@
 public class SimpleDotComTestDrive {
+
   public static void main (String [] args) {
+    
     SimpleDotCom dot = new SimpleDotCom();
 
     int[] locations = {2, 3, 4};
